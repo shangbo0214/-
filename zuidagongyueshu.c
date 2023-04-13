e34f877708a881ec7701643b9a6b1c50a97f56b2
shangbo0214 <130465035+shangbo0214@users.noreply.github.com>
2023-04-13 20:26:49 +0800

    Add files via upload

---
+++ zuidagongyueshu.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	int i = 1;
+	printf("请输入两个数字\n");
+	scanf("%d%d",&num1,&num2);
+	while(i<=num1||i<=num2)
+	{
+		if(num1 %i == 0 && num2 %i == 0)
+		  {
+		   num3 = i;
+		  }
+		 i++;		 
+	}
+	printf("%d\n",num3);
+	return 0;
+}
